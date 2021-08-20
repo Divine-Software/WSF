ac32a3621b7ed96038b1119559ff8c9c941790d0
Martin Blom <martin@blom.org>
2021-08-20 08:41:19 +0200

    encodeCredentials/decodeCredentials are now static.

--- uri/src/auth-schemes/basic.ts
+++ uri/src/auth-schemes/basic.ts
@@ -13,11 +13,11 @@ export class BasicAuthScheme extends AuthScheme<BasicCredentials> {
         super(scheme);
     }
 
-    encodeCredentials(credentials: BasicCredentials): string {
+    static encodeCredentials(credentials: BasicCredentials): string {
         return b64Encode(`${credentials.identity}:${credentials.secret}`);
     }
 
-    decodeCredentials(credentials?: string): BasicCredentials | undefined {
+    static decodeCredentials(credentials?: string): BasicCredentials | undefined {
         const [, username, password] = /([^:]*):?(.*)/.exec(b64Decode(credentials ?? '')) ?? [undefined, undefined, undefined];
 
         return username !== undefined && password !== undefined ? new BasicCredentials(username, password) : undefined;
@@ -27,11 +27,11 @@ export class BasicAuthScheme extends AuthScheme<BasicCredentials> {
         const credentials = await this.getCredentials({ mode: 'retrieve', authScheme: this, challenge, request });
         const proxyHeader = challenge?.isProxyHeader() ?? this.proxy;
 
-        return credentials ? new Authorization(`${this.scheme} ${this.encodeCredentials(credentials)}`, proxyHeader) : undefined;
+        return credentials ? new Authorization(`${this.scheme} ${BasicAuthScheme.encodeCredentials(credentials)}`, proxyHeader) : undefined;
     }
 
     async verifyAuthorization<T extends Authorization | undefined>(authorization: T, request?: AuthSchemeRequest, _payload?: Uint8Array): Promise<T> {
-        const untrusted = this.decodeCredentials(this.assertCompatibleAuthHeader(authorization)?.credentials);
+        const untrusted = BasicAuthScheme.decodeCredentials(this.assertCompatibleAuthHeader(authorization)?.credentials);
 
         if (!untrusted) {
             throw new AuthSchemeError(`No credentials provided`, await this.createChallenge(authorization));
@@ -43,7 +43,7 @@ export class BasicAuthScheme extends AuthScheme<BasicCredentials> {
             throw new AuthSchemeError(`User ${untrusted.identity} not found`, await this.createChallenge(authorization));
         }
 
-        if (!this.safeCompare(this.encodeCredentials(untrusted), this.encodeCredentials(trusted))) {
+        if (!this.safeCompare(BasicAuthScheme.encodeCredentials(untrusted), BasicAuthScheme.encodeCredentials(trusted))) {
             throw new AuthSchemeError(`Invalid password`, await this.createChallenge(authorization));
         }
 
